6e5551a383fca9a23db436ba22a073c0bdeff2d7
yeleia <2406865312@qq.com>
2018-08-08 11:04:25 +0800

    first commit

---
+++ src/main/java/org/wingstudio/sports/SportsApplication.java
@@ -0,0 +1,12 @@
+package org.wingstudio.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsApplication.class, args);
+    }
+}
